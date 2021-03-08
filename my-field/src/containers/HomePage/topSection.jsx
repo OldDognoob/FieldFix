860f119b69f6e63105e0448a5d11a161e6473efc
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-03-08 12:49:52 +0200

    wip

--- my-field/src/containers/HomePage/topSection.jsx
+++ my-field/src/containers/HomePage/topSection.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import styled from "styled-components";
 
- function TopSection() {
+import TopSectionBackgroundImg from "../../images/landing-page.jpg";
+
+const TopSectionContainer = styled.div`
+width: 100%;
+height: 600px;
+background:url(${TopSectionBackgroundImg});
+background-position: 0px -50px;
+background-size: cover; // fits the image accordingly to the fitting space
+`;
+
+ function TopSection(props) {
     return (
         <div>
             
