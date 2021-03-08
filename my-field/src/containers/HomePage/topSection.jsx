eb3fd31fde4cb50cea190a525a0a20eb8fd94599
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-03-08 15:23:56 +0200

    wip-adding-img

--- my-field/src/containers/HomePage/topSection.jsx
+++ my-field/src/containers/HomePage/topSection.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from "styled-components";
 
 import TopSectionBackgroundImg from "../../images/services/landing-page1.jpg";
+import TheExpertiseImg from "../../images/services/Expertise.png";
 
 const TopSectionContainer = styled.div`
 width: 100%;
@@ -19,9 +20,31 @@ display: flex;
 flex-direction: column;
 `;
 
+const TopSectionInnnerContainer = styled.div`
+width:100%;
+height:100%;
+display: flex;
+align-items: center;
+justify-content: space-evenly;
+`;
+
+const StandoutImg= styled.div`
+width: 44em;
+height: 34em;
+
+img{
+    width:100%;
+    height:100%;
+}
+`;
+
  function TopSection(props) {
     return<TopSectionContainer>
-        <BackgroundFilter>YEA</BackgroundFilter>
+        <BackgroundFilter>
+            <StandoutImg>
+                <img src={TheExpertiseImg} alt="An expert in your doorway"/>
+            </StandoutImg>
+        </BackgroundFilter>
     </TopSectionContainer>
 }
 export default TopSection;
