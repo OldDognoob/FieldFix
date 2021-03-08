cffe5913c5b3410dca488010bb577d8c062634e5
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-03-08 13:13:06 +0200

    wip-background

--- my-field/src/containers/HomePage/topSection.jsx
+++ my-field/src/containers/HomePage/topSection.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import styled from "styled-components";
 
-import TopSectionBackgroundImg from "../../images/services/landing-page.jpg";
+import TopSectionBackgroundImg from "../../images/services/landing-page1.jpg";
 
 const TopSectionContainer = styled.div`
 width: 100%;
-height: 600px;
+height: 800px;
 background:url(${TopSectionBackgroundImg});
-background-position: 0px -50px;
+background-position: 0px -150px;
 background-size: cover; // fits the image accordingly to the fitting space
 `;
 
