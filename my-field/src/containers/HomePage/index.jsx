5f72a710447fd13e7b9937aec554a79677ed5182
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-03-08 11:59:02 +0200

    wip

--- my-field/src/containers/HomePage/index.jsx
+++ my-field/src/containers/HomePage/index.jsx
@@ -1,8 +1,7 @@
 import React from "react";
+import {PageContainer} from "../../components/pageContainer";
 
-function HomePage(){
-    return(
-
-    )
+function HomePage(props){
+    return <PageContainer>Hello</PageContainer>;
 }
 export default HomePage;
